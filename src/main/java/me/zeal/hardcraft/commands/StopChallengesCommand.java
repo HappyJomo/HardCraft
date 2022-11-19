a1817de3b4fd883425073e84c82d64831f17a777
HappyJomo <jonathanmoller2005@gmail.com>
2022-11-19 23:16:17 +0100

    Command to stop challenges

---
+++ src/main/java/me/zeal/hardcraft/commands/StopChallengesCommand.java
@@ -0,0 +1,34 @@
+package me.zeal.hardcraft.commands;
+
+import me.zeal.hardcraft.challenge.ChallengeManager;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class StopChallengesCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String cmd, String[] args) {
+        if (!(sender instanceof Player)) {
+            return false;
+        }
+
+        Player player = (Player) sender;
+        ChallengeManager challengeManager = ChallengeManager.getChallengeManager();
+        if (cmd.equalsIgnoreCase("stopchallenges")) {
+            if (!challengeManager.isStarted()) {
+                player.sendMessage(ChatColor.RED + "no");
+                return true;
+            }
+        }
+
+        challengeManager.setStarted(false);
+        Bukkit.getScheduler().cancelTask(challengeManager.getTimerId());
+        player.sendMessage("ok we stop");
+
+        return true;
+    }
+}
